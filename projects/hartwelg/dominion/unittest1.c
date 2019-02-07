4339f7785aac772d8d4fcc5ee8575681f2839191
Gauge Hartwell <hartwelg@flip2.engr.oregonstate.edu>
2019-02-06 17:58:38 -0800

    removed quiz directory

---
+++ projects/hartwelg/dominion/unittest1.c
@@ -0,0 +1,18 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+
+#define DEBUG 0
+#define NOISY_TEST 1
+
+int testGetCost()
+{
+
+}
+int main()
+{
+	return 0;
+}
