8e5f3951c73561e63088090c618cd9abf93f9a47
Gray Lunn <lunng@oregonstate.edu>
2019-03-11 16:46:02 -0700

    Added random tests

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -49,28 +49,60 @@ public class UrlValidatorTest extends TestCase {
          }
       }
    }
-   
-   public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
-      String[] invalidSchemes = {};// TODO: add 3 more schemes each
-      String[] invalidAuthorities = {};// TODO: add 3 more authorities each
-      String[] invalidPorts = {};            // TODO: add 3 more ports each
-      String[] invalidPaths = {};                // TODO: add 3 more valid paths each 
-      String[] invalidQueries = {}       // TODO: add 2 more queries each
-      for(scheme:invalidSchemes){
-         for(auth:invalidAuthorities){
-            for(port:invalidPorts){
-               for(path:invalidPaths){
-                  for(query:invalidQueries){
-                     String testURL = scheme + auth + port + path + query;
-                     assertTrue(isValid(testURL) == false);
-                  }
-               }
-            }
-         }
-      }
 
-   }
+    public void testYourSecondPartition(){
+        //You can use this function to implement your Second Partition testing
+        String[] validSchemes = {"http://", "https://", ""};// TODO: add 3 more schemes each
+        String[] validAuthorities = {"www.google.com", "www.yahoo.com", "www.worldtangsoodo.com"}; // TODO: add 3 more authorities each
+        String[] validPorts = {"", ":120", ":640"};             // TODO: add 3 more ports each
+        String[] validPaths = {"/$41", "/error", "/software/htp/cics/index.html"};                 // TODO: add 3 more valid paths each
+        String[] validQueries = {"?action=view"}        // TODO: add 2 valid queries each
+        String[] invalidSchemes = {"errno", "ftp:", "://"};       // TODO: add 3 more schemes each
+        String[] invalidAuthorities = {"badname.badname", ".com.google", "none"};   // TODO: add 3 more authorities each
+        String[] invalidPorts = {"0", ":1000222", ":port"};         // TODO: add 3 more ports each
+        String[] invalidPaths = {"//", "path??", "probablybadlol///"};         // TODO: add 3 more valid paths each
+        String[] invalidQueries = {"xyz::#$%^"}        // TODO: add 2 more queries each
+        // build 5 strings with only one invalid part for each invalid part, make sure that full string is invalid
+        for(scheme:invalidSchemes){
+            for(auth:invalidAuthorities){
+                for(port:invalidPorts){
+                    for(path:invalidPaths){
+                        for(query:invalidQueries){
+                            String testURL = scheme    + validAuthorities[(int) (Math.random() *validAuthorities.length)]
+                                    + validPorts[(int) (Math.random() *validPorts.length)]
+                                    + validPaths[(int) (Math.random() *validPaths.length)]
+                                    + validQueries[(int) (Math.random() *validQueries.length)] ;
+                            assertTrue(isValid(testURL) == false);
+                            testURL = validSchemes[(int) (Math.random() *validSchemes.length)]
+                                    + auth
+                                    + validPorts[(int) (Math.random() *validPorts.length)]
+                                    + validPaths[(int) (Math.random() *validPaths.length)]
+                                    + validQueries[(int) (Math.random() *validQueries.length)] ;
+                            assertTrue(isValid(testURL) == false);
+                            testURL = validSchemes[(int) (Math.random() *validSchemes.length)]
+                                    + validAuthorities[(int) (Math.random() *validAuthorities.length)]
+                                    + port
+                                    + validPaths[(int) (Math.random() *validPaths.length)]
+                                    + validQueries[(int) (Math.random() *validQueries.length)] ;
+                            assertTrue(isValid(testURL) == false);
+                            testURL = validSchemes[(int) (Math.random() *validSchemes.length)]
+                                    + validAuthorities[(int) (Math.random() *validAuthorities.length)]
+                                    + validPorts[(int) (Math.random() *validPorts.length)]
+                                    + path
+                                    + validQueries[(int) (Math.random() *validQueries.length)] ;
+                            assertTrue(isValid(testURL) == false);
+                            testURL = validSchemes[(int) (Math.random() *validSchemes.length)]
+                                    + validAuthorities[(int) (Math.random() *validAuthorities.length)]
+                                    + validPorts[(int) (Math.random() *validPorts.length)]
+                                    + validPaths[(int) (Math.random() *validPaths.length)]
+                                    + query;
+                            assertTrue(isValid(testURL) == false);
+                        }
+                    }
+                }
+            }
+        }
+    }
    //You need to create more test cases for your Partitions if you need to 
    
    public void testIsValid()
